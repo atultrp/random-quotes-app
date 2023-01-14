a74d6862c1a2ef917e27358b8448e46fdaca520f
Atul Tripathi <atul31815@gmail.com>
2023-01-14 13:28:51 +0530

    new quotes api implementation done

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,33 @@ import { useEffect, useState } from 'react';
 export default function Home() {
 
   const apiLink = "https://type.fit/api/quotes"
-  const [randomColor, setRandomColor] = useState("green")
+  const [randomColor, setRandomColor] = useState("rose")
+  const [quoteData, setQuoteData] = useState()
+  const [quote, setQuote] = useState()
+  const [shareQuote, setShareQuote] = useState()
 
-  const randomColorsArr = ["red", "${randomColor}", "green", "blue", "gray", "pink", "orange", "teal", "cyan", "purple", "violet", "yellow"]
+  const randomColorsArr = ["red", "rose", "green", "blue", "gray", "pink", "orange", "teal", "cyan", "purple", "violet", "yellow"]
 
-  let colorIndex = Math.floor(Math.random() * (randomColorsArr.length + 1))
   useEffect(() => {
-    setRandomColor(randomColorsArr[colorIndex])
+    fetch(apiLink)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setQuoteData(data)
+      })
   }, [])
-  console.log("bg-" + randomColor + "-800", randomColorsArr.length)
 
+  const handleNewQuote = () => {
+    let colorIndex = Math.floor(Math.random() * (randomColorsArr.length))
+    setRandomColor(randomColorsArr[colorIndex])
+
+    if (quoteData) {
+      let quoteIndex = Math.floor(Math.random() * (quoteData.length))
+      setQuote(quoteData[quoteIndex])
+    }
+  }
+
+  const shareQuoteDummy = "People often say that motivation doesn't last. Well, neither does bathing. That's why we recommend it daily. - Zig Ziglar from https://blog-like-star.vercel.app"
 
   return (
     <>
@@ -31,39 +48,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={`${"bg-" + randomColor + "-800"} h-screen text items-center flex`}>
-        <div className={`mx-auto w-[650px] px-12 py-8 ${"text-" + randomColor + "-800"} bg-white rounded-tl-3xl rounded-br-3xl shadow-lg shadow-${randomColor}-900 font-mono`}>
+      <main className={` bg-${randomColor}-800 h-screen text items-center flex`}>
+        <div className={`mx-auto w-[650px] px-12 py-8 text-${randomColor}-800 bg-white rounded-tl-3xl rounded-br-3xl shadow-lg shadow-${randomColor}-900 font-mono`}>
           <div className='flex items-center'>
-
             <div className='text-3xl flex'>
               <span>
                 <FaQuoteLeft />
               </span>
               <span className='ml-4 '>
-                People often say that motivation doesn’t last. Well, neither does bathing. That’s why we recommend it daily.
+                {quote?.text || "People often say that motivation doesn't last. Well, neither does bathing. That's why we recommend it daily."}
               </span>
             </div>
           </div>
 
           <div className='text-end mt-2'>
-            <span className='font-light text-lg italic border-b-2 w-fit'>
-              - Zig Ziglar
+            <span className={`font-light text-lg italic border-b-2 w-fit border-b-${randomColor}-800`}>
+              - {quote?.author || "Zig Ziglar"}
             </span>
           </div>
 
           <div className='flex justify-between mt-6'>
             <div >
               <WhatsappShareButton
-                url={'https://blog-like-star.vercel.app'} >
-                <FaWhatsappSquare className='text-5xl text-${randomColor}-800 mr-1 hover:scale-125 ease-in-out duration-300' />
+                url={shareQuote || shareQuoteDummy} >
+                <FaWhatsappSquare className='text-5xl mr-1 hover:scale-125 ease-in-out duration-300' />
               </WhatsappShareButton>
               <TwitterShareButton
-                url={'https://blog-like-star.vercel.app'} >
-                <FaTwitterSquare className='text-5xl text-${randomColor}-800  hover:scale-125 ease-in-out duration-300' />
+                url={shareQuote || shareQuoteDummy}
+              >
+                <FaTwitterSquare className={`text-5xl hover:scale-125 ease-in-out duration-300`} />
               </TwitterShareButton>
             </div>
             <div>
-              <button className='bg-blue-800 text-white px-3 py-2 rounded-md font-semibold hover:scale-125 ease-in-out duration-300'>
+              <button
+                className={`bg-${randomColor}-800 text-white px-3 py-2 rounded-md font-semibold hover:scale-125 ease-in-out duration-300`}
+                onClick={handleNewQuote}
+              >
                 New Quote
               </button>
             </div>
